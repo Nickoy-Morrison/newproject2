cf3d99a9792610c13b755f788e93f12d74ce7bb4
Nickoy Morrison <43710194+Nickoy-Morrison@users.noreply.github.com>
2021-04-25 17:52:10 -0500

    Add files via upload

--- app/config.py
+++ app/config.py
@@ -9,9 +9,12 @@ class Config(object):
     UPLOAD_VPROFILE = os.environ['UPLOAD_VPROFILE'] = 'static/uploads/profile' 
     UPLOAD_VCARPHOTO = os.environ['UPLOAD_VCARPHOTO'] = 'static/uploads/carPhoto'
     
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://znumtfhciuflgi:d301b7fc3b692b3090f998668556087f25ce7ba07032c3632e6e554eb477b847@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d78kmplhv5g4ht'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://lxoeryfjfttxwv:b238acaf7ee1ea8c6c1b44765c2f0377b9079332191a6593c7acee51153da13f@ec2-52-205-3-3.compute-1.amazonaws.com:5432/da1riu584cl0p7'
     #'postgresql://project2:mkm2021@localhost/project2'
+    #'postgresql://qjfvlgfydfwhrs:7b9a737d0dc3370cbc16032a0d91e5721c8449e71064ca41e63dbe8c7a2f6a78@ec2-34-225-167-77.compute-1.amazonaws.com:5432/d6b1u73jbtm9ht'
+    #
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
 class DevelopmentConfig(Config):
